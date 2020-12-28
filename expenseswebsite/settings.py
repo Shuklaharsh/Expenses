e14451e1048624d23a511129e531e5920b37caa5
Shuklaharsh <43582783+Shuklaharsh@users.noreply.github.com>
2020-12-28 12:16:20 +0530

    Update settings.py

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_%jh63201wi_tz#129(2cfrbk9^7ebzye)wnivcrw@43-xcgl+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://expenses-i.herokuapp.com/']
 
