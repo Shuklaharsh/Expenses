37210725cf5b505af1f177a9e86567bf8e14dccc
Sagar Rajput <43861831+Shaanrj@users.noreply.github.com>
2020-12-28 13:11:48 +0530

    Update views.py

--- userincome/views.py
+++ userincome/views.py
@@ -29,7 +29,10 @@ def index(request):
     paginator = Paginator(income, 5)
     page_number = request.GET.get('page')
     page_obj = Paginator.get_page(paginator, page_number)
-    currency = UserPreference.objects.get(user=request.user).currency
+    try:
+        currency = UserPreference.objects.get(user_id=request.user).currency
+    except UserPreference.DoesNotExist:
+        currency = 'USD - United States Dollar'
     context = {
         'income': income,
         'page_obj': page_obj,
